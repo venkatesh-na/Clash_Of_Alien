986f81a2dab605bcdc3fbc6c949232d267778951
venkatesh-na <79442047+venkatesh-na@users.noreply.github.com>
2022-08-03 10:19:25 +0530

    Removed long press feature

--- script.js
+++ script.js
@@ -462,11 +462,11 @@ let player = document.querySelector(".box.player2")
 let intervalForLongpress;
 allControllButtons.forEach(btn => {
     btn.addEventListener("click",handleSingleClick)
-    btn.addEventListener("touchstart",handleMobileControll)
-    btn.addEventListener("touchend",()=>{
-        console.log("end")
-       clearInterval(intervalForLongpress);
-    })
+    //btn.addEventListener("touchstart",handleMobileControll)
+   // btn.addEventListener("touchend",()=>{
+     //   console.log("end")
+     //  clearInterval(intervalForLongpress);
+ //})
     //when i double click on button it acts weared so i added if we double click on button do nothing but double click does not work on movbile
     // btn.addEventListener("dblclick",()=>{
     //     console.log("dbl")
